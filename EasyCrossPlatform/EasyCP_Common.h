4626bd90257035adb65d57456ab159639e89a24e
年迈的老秋风Windy <cxcyhdyx@sina.com>
2017-11-05 21:14:57 -0500

    Add a Platform Definition Statement
    To prepare for future sockets and system file development, we have defined EASYCROSSPLATFORM_PLATFORM_WINDOWS/LINUX on our Common Header FIle!

--- EasyCrossPlatform/EasyCP_Common.h
+++ EasyCrossPlatform/EasyCP_Common.h
@@ -1,6 +1,13 @@
 #ifndef XSYDCommonHeadersFile
 	#define XSYDCommonHeadersFile
 	#define NULL 0
+	//Define系统类型
+	#define EASYCROSSPLATFORM_VERSION 0;
+	#if defined(linux) || defined(_UNIX) || defined(__LINUX__){
+		#define EASYCROSSPLATFORM_PLATFORM_LINUX true
+	#elif defined(__WINDOWS_) || defined(_WIN32) {
+		#define EASYCROSSPLATFORM_PLATFORM_WINDOWS true
+	#endif
 	#include <cassert> //Debug类
 	#include <exception> //Exception异常类
 	#include <stdexcept> //StdException通用异常类
